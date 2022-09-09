a3a7ff03faca644fdaeb2ab798024de4295dc9a1
David Kollinger <david.kollinger@siemens.com>
2022-09-09 15:19:04 +0200

    Add CODE_OF_CONDUCT.md, CONTRIBUTION and license warning in code

--- src/control/code.js
+++ src/control/code.js
@@ -1,3 +1,7 @@
+// Copyright 2022 Siemens AG. This file is subject to the terms and conditions of the MIT License. See LICENSE file in the top-level directory.
+//
+// SPDX-License-Identifier: MIT
+
 
 ////////////////////////////////////////////
 // Helper functions
